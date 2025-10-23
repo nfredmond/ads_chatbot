32db31beac2c022c054bae19f3d3ffd467beab8d
Nat Ford <natford@example.com>
2025-10-22 21:13:24 -0700

    Fix spacing between password field and Sign In button on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -77,7 +77,7 @@ export default function LoginPage() {
               />
             </div>
           </CardContent>
-          <CardFooter className="flex flex-col space-y-4">
+          <CardFooter className="flex flex-col space-y-4 pt-6">
             <Button type="submit" className="w-full" disabled={loading}>
               {loading ? 'Signing in...' : 'Sign In'}
             </Button>
